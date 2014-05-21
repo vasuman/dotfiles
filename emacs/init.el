a5d12ace790628cfd686765060f07aedf9182a13
Ravichandran <vasumanr@u7427ea171ba75373514b.ant.amazon.com>
2014-05-21 18:41:12 +0530

    Package install

--- emacs/init.el
+++ emacs/init.el
@@ -1,33 +1,48 @@
+(require 'cl-lib)
+
+;; Check and install packages
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/") 
+                         ("marmalade" . "http://marmalade-repo.org/packages/") 
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(defvar my-packages '(cider haskell-mode wrap-region expand-region))
+(defun packages-installed-p (ps) (cl-every 'package-installed-p ps))
+
+(require 'package)
+
+(package-initialize)
+
+(unless (packages-installed-p my-packages)
+  (package-refresh-contents)
+  (dolist (p my-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+;; Keybindings
 (global-set-key (kbd "C-=") 'er/expand-region)
+
+;; Hooks
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(backup-by-copying t)
- '(backup-directory-alist (quote (("." . "~/.saves"))))
- '(c-basic-offset 4)
- '(custom-enabled-themes (quote (whiteboard)))
- '(delete-old-versions t)
- '(ido-enable-flex-matching t)
- '(ido-everywhere t)
- '(ido-mode (quote both) nil (ido))
- '(inhibit-startup-screen t)
- '(initial-scratch-message "")
- '(kept-new-versions 6)
- '(keyboard-coding-system (quote utf-8-unix))
- '(menu-bar-mode nil)
- '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/"))))
- '(tool-bar-mode nil)
- '(version-control t)
- '(wrap-region-global-mode t)
- '(yas/global-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;; Settings
+(setq backup-by-copying t)
+(setq backup-directory-alist '(("." . "~/.saves")))
+(setq c-basic-offset 4)
+(setq custom-enabled-themes '(whiteboard))
+(setq delete-old-versions t)
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message "")
+(setq kept-new-versions 6)
+(setq keyboard-coding-system 'utf-8-unix)
+(setq version-control t)
+(wrap-region-global-mode t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(ido-mode t)
+(ido-everywhere t)
+(windmove-default-keybindings 'meta)
+(global-auto-revert-mode 1)
+
+(setq fill-column 80)
+(setq-default truncate-lines t)
