3cee01b72195f6213ac115ba7f9951356d08b717
Ravichandran <vasumanr@u7427ea171ba75373514b.ant.amazon.com>
2014-06-27 12:55:35 +0530

    Emacs UI

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,14 +4,10 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/") 
                          ("marmalade" . "http://marmalade-repo.org/packages/") 
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
-
 (defvar my-packages '(cider haskell-mode wrap-region expand-region magit))
 (defun packages-installed-p (ps) (cl-every 'package-installed-p ps))
-
 (require 'package)
-
 (package-initialize)
-
 (unless (packages-installed-p my-packages)
   (package-refresh-contents)
   (dolist (p my-packages)
@@ -25,7 +21,7 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
-;; Settings
+;; Preferences
 (setq backup-by-copying t)
 (setq backup-directory-alist '(("." . "~/.saves")))
 (setq c-basic-offset 4)
@@ -36,6 +32,8 @@
 (setq kept-new-versions 6)
 (setq keyboard-coding-system 'utf-8-unix)
 (setq version-control t)
+(setq fill-column 80)
+(setq-default truncate-lines t)
 (wrap-region-global-mode t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -43,6 +41,19 @@
 (ido-everywhere t)
 (windmove-default-keybindings 'meta)
 (global-auto-revert-mode 1)
+(set-face-attribute 'default nil :height 150)
 
-(setq fill-column 80)
-(setq-default truncate-lines t)
+;; Custom -- don't edit
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (deeper-blue))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
