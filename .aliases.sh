d5bb8f9d5d0f99ab266e96ee122d95f169a626c6
Vasuman <vasuman@amazon.com>
2016-03-29 19:57:39 +0530

    Git aliases

--- .aliases.sh
+++ .aliases.sh
@@ -7,6 +7,11 @@ alias la='ls -al'
 alias e='emacs -nw'
 alias v='vim'
 
+alias gs='git status'
+alias gc='git commit'
+
 alias serve='python2 -m SimpleHTTPServer'
+
 alias commit-dot='cd .dot && git add . && git commit -a && git push && cd -'
+
 alias npm-path='export PATH=$(pwd)/node_modules/.bin:$PATH'
